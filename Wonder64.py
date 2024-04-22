96347b3528da23514d01f0fbd8c56853e1647e89
Cat-san <84363801+Catdevzsh@users.noreply.github.com>
2024-04-22 15:11:04 -0700

    Create Wonder64.py

---
+++ Wonder64.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+
+# Set window title
+root.title("Wonder 64 Build 4.22.24 0.1$")
+
+# Set fixed window size
+root.geometry("600x400")
+
+# Disable resizing the window
+root.resizable(False, False)
+
+# Start the GUI event loop
+root.mainloop()
+## [Credit to hal labs x nintendo ] 
+
+# OG by pj64 team 
